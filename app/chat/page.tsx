100e1d60461bb0a244e5a1519d2b8dd1006e96c3
Rodion Krjutškov <rodion.krjutskov@gmail.com>
2025-01-19 16:04:33 +0200

    Add user messages handling in chat

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,9 +1,41 @@
-import styles from '../styles.module.css'
+'use client'
 
-export default async function Chat() {
+import { useActionState } from "react"
+import styles from './styles.module.css'
+
+interface ChatMessage {
+    role: "user" | "assistant"
+    content: string
+}
+
+function addUserMessage(previousState, formData) {
+    const userMessage = formData.get('userMessage')
+    if (userMessage !== '') {
+        return [{ role: 'user', content: userMessage }, ...previousState]
+    }
+    return previousState
+}
+
+function addAssistantMessage(previousState, formData) {
+    
+}
+
+function formatMessages(messages: ChatMessage[]) {
+    return messages.map((message, index) => <div key={index} className={styles['message-' + message.role]}>{message.content}</div>)
+}
+
+export default function Chat() {
+    const [messages, formAction] = useActionState(addUserMessage, []);
+
     return (
-        <div className={styles.container}>
-            <h1>You have chat access</h1>
+        <div className={styles['page-container']}>
+            <div className={styles['messages-container']}>
+                {formatMessages(messages)}
+            </div>
+            <form action={formAction} className={styles['chat-footer']}>
+                <input name='userMessage' className={styles['chat-input']}></input>
+                <button className={styles['chat-button']}>Send</button>
+            </form>
         </div>
     )
 }
