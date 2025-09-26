bd1672d735e422bab97d3305daeb9be25f42542a
Rodion Krjutškov <rodion.krjutskov@gmail.com>
2025-09-26 19:32:15 +0300

    Change video hosting

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,18 +1,11 @@
-import { list } from '@vercel/blob'
 import styles from '@/styles/video.module.css'
 
-export default async function Video({ fileName }) {
-    const { blobs } = await list({
-        prefix: fileName,
-        limit: 1,
-    })
-    const { url } = blobs[0]
-
+export default function Video({ fileName }) {
     return (
         <div className={styles['video-section']}>
             <div className={styles['video-container']}>
-                <video controls preload="none" aria-label="Video player" poster='images/video-poster.png'>
-                    <source src={url} type="video/mp4" />
+                <video controls preload="none" aria-label="Video player" poster='demo/poster.png'>
+                    <source src="/demo/video.mp4" type="video/mp4" />
                     Your browser does not support the video tag.
                 </video>
             </div>
