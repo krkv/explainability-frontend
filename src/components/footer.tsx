3c46c04e7fa68bcb20e53f31c8c68e593a684e11
Rodion Krjutškov <rodion.krjutskov@gmail.com>
2025-09-26 13:03:52 +0300

    Add character escape

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import styles from '@/styles/footer.module.css'
 export default function Footer() {
     return (
         <footer className={styles['footer']}>
-            <p className={styles['footer-text']}>This project was supported by the European Union's Horizon 2020 research and innovation programme under grant agreement No 952060 (Trust AI)</p>
+            <p className={styles['footer-text']}>This project was supported by the European Union&apos;s Horizon 2020 research and innovation programme under grant agreement No 952060 (Trust AI)</p>
         </footer>
     )
 }
