41c7a9bd4c96560c7505696f206d033b3de45b73
Rodion Krjutškov <rodion.krjutskov@gmail.com>
2025-10-06 16:01:43 +0300

    Update login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,21 @@
+import { Sansation } from 'next/font/google'
+import classNames from 'classnames'
 import styles from '@/styles/login.module.css'
 import { validateForm } from '@/actions/auth'
 import Image from 'next/image'
 import assistantIcon from '@/assets/claire-b.png'
+
+const sansation = Sansation({
+    subsets: ['latin'],
+    weight: ['300', '400', '700']
+})
 
 function AccessForm() {
     return (
         <form action={validateForm} className={styles['form-container']}>
             <input name='email' type='email' className={styles['access-input']} placeholder='Your email'></input>
             <input name='password' type='password' className={styles['access-input']} placeholder='Your password'></input>
-            <button className={styles['access-button']}>Demo Access</button>
+            <button className={styles['access-button']}>Access Demo</button>
         </form>
     )
 }
@@ -17,7 +24,7 @@ export default function LoginPage() {
     return (
         <div className={styles['page-container']}>
             <Image src={assistantIcon} alt="Assistant icon" width={64} height={64} />
-            <h1>Explainability Assistant</h1>
+            <h1 className={classNames(styles['page-title'], sansation.className)}>Explainability Assistant</h1>
             <AccessForm />
         </div>
     )
